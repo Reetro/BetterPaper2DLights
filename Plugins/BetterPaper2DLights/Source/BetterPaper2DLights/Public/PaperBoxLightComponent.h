9e55eff861967b6e493e0a34752607a0da9ab3c2
Reetro <thegamerboy7@gmail.com>
2020-04-14 22:31:21 -0600

    started work on the paper box light

---
+++ Plugins/BetterPaper2DLights/Source/BetterPaper2DLights/Public/PaperBoxLightComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/LocalLightComponent.h"
+#include "PaperBoxLightComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETTERPAPER2DLIGHTS_API UPaperBoxLightComponent : public ULocalLightComponent
+{
+	GENERATED_BODY()
+	
+};
